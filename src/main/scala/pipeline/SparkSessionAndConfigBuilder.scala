df969e0efaa8df909f507fafe93e9cfd5a9fa5b8
Josef Habdank <jahabdank@gmail.com>
2018-10-25 19:33:38 +0200

    updated for spark-submit

--- src/main/scala/pipeline/SparkSessionAndConfigBuilder.scala
+++ src/main/scala/pipeline/SparkSessionAndConfigBuilder.scala
@@ -6,7 +6,11 @@ import org.apache.spark.sql.SparkSession
 package object SparkSessionAndConfigBuilder {
   def setupSpark(): SparkSession = {
     val appName = "pipeline"
-    val conf    = new SparkConf().setAppName(appName).setMaster("local[*]")
+    var conf    = new SparkConf()
+    if (conf.contains("spark.master")) conf.setAppName(appName)
+    else conf.setAppName(appName).setMaster("local[*]")
+
+    println(s"Spark conf:\n${conf.getAll.mkString("\n")}")
 
     SparkSession.builder().config(conf).appName(appName).getOrCreate()
   }
